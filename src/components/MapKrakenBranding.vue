4c71ad1c44b1c71edef3d34a7ecbff09a3b225ee
tyler mccracken <tylermccracken@tylers-MacBook-Pro.local>
2025-03-10 12:07:33 -0400

    new logo and layout

--- src/components/MapKrakenBranding.vue
+++ src/components/MapKrakenBranding.vue
@@ -1,10 +1,27 @@
 <template>
-  <img alt="Vue logo" src="../assets/MapKraken logo_D2.jpg">
+  <div class="branding-container">
+    <img class="fit-screen" alt="Vue logo" src="../assets/MapKraken logo_C.jpg">
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'MapKrakenBranding',
 }
 </script>
+
+<style scoped>
+.branding-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fit-screen {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
